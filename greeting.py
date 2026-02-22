795e8cc5c5ebf4c9862bf174e8080bdfc7b35d0e
SKSIRVI206 <sksirvi206@gmail.com>
2026-02-22 17:46:19 +0530

    Initial commit

---
+++ greeting.py
@@ -0,0 +1,4 @@
+firstName = input('Enter your first name: ')
+lastName = input('Enter your last name: ')
+fullName = f'Hello {firstName} {lastName}! welcome to python program'
+print(fullName)
---
+++ index.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("Enter the first number: "))
+secondNumber = int(input("Enter the second number: "))
+addition = firstNumber + secondNumber
+subtraction = firstNumber - secondNumber
+multiplication = firstNumber * secondNumber
+division = firstNumber / secondNumber
+print(f'Addition: {addition}')
+print(f'Subtraction: {subtraction}')
+print(f'Multiplication: {multiplication}')
+print(f'Division: {division}')
